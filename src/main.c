f632ab17118a1c2fbe1310e78311c2dea1e98969
felipidis <felipekdra@gmail.com>
2018-05-20 18:59:00 -0300

    Atualizado para english

--- src/main.c
+++ src/main.c
@@ -6,61 +6,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-void cpu() { // função que testa o processador
-	for(;;){ // loop infinito para o teste de processador
+void cpu() { // Function that tests the processor
+	for(;;){ // Infinite loop for the test of processor
 	}
 }
 
-void cpu_mem() { // função que testa a memória e o processador
-	for(;;){ // loop infinito para o teste de processador
-		malloc(sizeof(int)); // aloca um inteiro a cada loop para o teste de memória
+void cpu_mem() { // Function that tests the memory and processor
+	for(;;){ // Infinite loop for the test of processor
+		malloc(sizeof(int)); // Allocates an integer to each loop for the memory test
 	}
 }
-// função que verifica se duas strings são iguais
+// Function that checks if two strings are equal
 int string_equals(char *string1, char *string2) {
 	return *string1 != *string2 ? 0 : (*string1 == '\0' && *string2 == '\0') ? 1 : string_equals(string1 + 1, string2 + 1);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-	int pid = fork(); // identificador do processo
-	int i = 0; // i -> conta os segundos de execução do processo filho dentro do processo pai para interromper aos 10 segundos
-	// sys -> armazena o comando do sistema que mostra o pid, porcentagem da CPU e/ou a quantidade de bytes alocados
-	// aux -> armazena o valor do pid do filho convertido para string 
+	int pid = fork(); // Process identifier
+	int i = 0; // i -> Account the child process execution seconds within the parent process to stop at 10 seconds
+	// sys -> Stores the system command that shows the pid, CPU percentage and / or number of bytes allocated
+	// aux -> Stores the pid's value of the converted child to string
 	char sys[100], aux[100]; 
-	if(pid < 0) { /* se o fork não funcionou */
+	if(pid < 0) { /* If the fork did not work */
 		perror ("Erro: ") ;
-		exit (-1) ; /* encerra o processo com código de erro -1 */ 		
-	} else if(pid == 0){ // se sou o processo filho
-		if(string_equals(argv[1], "ucp") == 1) { // se o paramêtro de argv[1] indicar que o programa deve testar apenas a cpu entra nesse if
-			cpu(); // chama a função cpu() que tem um loop infinito para forçar o processador
-		} else if(string_equals(argv[1], "ucp_mem") == 1) { // senão se o paramêtro indicar que o programar deve testar a cpu e a memória entra aqui
-			cpu_mem(); // chama a função cpu_mem que tem um loop infinito para forçar o processador e, em cada loop, uma chamada a função malloc allocando a quantidade de bytes de um inteiro para forçar a memória
+		exit (-1) ; /* Closes the process with error code -1 */ 		
+	} else if(pid == 0){ // If i'm the son process
+		if(string_equals(argv[1], "ucp") == 1) { // If the argv parameter [1] indicates that the program should test only the cpu enters this if
+			cpu(); // Calls the cpu () function that has an infinite loop to force the processor
+		} else if(string_equals(argv[1], "ucp_mem") == 1) { // Otherwise, if the parameter indicates that the program should test the cpu and the memory enters here
+			cpu_mem(); // Calls the cpu_mem function that has an infinite loop to force the processor and in each loop a call to the malloc function allocating the number of bytes of an integer to force the memory
 		}
-	} else { // se sou o pai
-		sprintf(aux, "%d", pid); // converte o pid do filho para string e passa esse valor pra variavel aux
-		if(string_equals(argv[1], "ucp") == 1) { // se for só o teste de processador
-			printf(" PID - CPU%%\n"); // printa na tela um header para melhor identificação visual dos valores
-			strcpy(sys, "ps -e -o pid,pcpu | grep "); // salva na variável 'sys' o comando que traz a tela os valores do pid e a porcentagem de cpu usada por um processo com o 'grep' para filtrar a partir do pid
-			strcat(sys, aux); // concatena a string sys o valor do pid a ser analisado
-		} else if(string_equals(argv[1], "ucp_mem") == 1) { // se o teste for de processador e de memória
-			printf(" PID - CPU%%\n"); // printa na tela um header para melhor identificação visual dos valores
-			strcpy(sys, "ps -e -o pid,pcpu | grep ");// salva na variável 'sys' o comando que traz a tela os valores do pid e a porcentagem de cpu usada por um processo com o 'grep' para filtrar a partir do pid
-			strcat(sys, aux);// concatena a string sys o valor do pid a ser analisado
-			strcat(sys, ";pmap ");// concatena a ela tbm um ';' para indicar fim de comando e 'pmap' para identificar quantos Kb de memoria estão sendo usadas pelo processo
-			strcat(sys, aux); // com o pid do processo o programa filtra qual processo deve ter a memoria monitorada
-			strcat(sys, " | grep -i total"); // com o uso do grep aqui, mostra apenas o total de memória usada
+	} else { // If I'm the father
+		sprintf(aux, "%d", pid); // Converts the child pid to string and passes this value to aux variable
+		if(string_equals(argv[1], "ucp") == 1) { // If it's just the processor test
+			printf(" PID - CPU%%\n"); // Prints on the screen a header for better visual identification of the values
+			strcpy(sys, "ps -e -o pid,pcpu | grep "); // Saves in the variable 'sys' the command that brings to the screen the values ​​of the pid and the percentage of cpu used by a process with 'grep' to filter from the pid
+			strcat(sys, aux); // Concatenates the sys string and the pid value to be parsed
+		} else if(string_equals(argv[1], "ucp_mem") == 1) { // If the test is processor and memory
+			printf(" PID - CPU%%\n"); // Prints on the screen a header for better visual identification of values
+			strcpy(sys, "ps -e -o pid,pcpu | grep ");// Saves in the variable 'sys' the command that brings to the screen the values ​​of the pid and the percentage of cpu used by a process with 'grep' to filter from the pid
+			strcat(sys, aux);// Concatenates the sys string and the pid value to parse
+			strcat(sys, ";pmap ");// Concatenates to her also a ';' to indicate command end and 'pmap' to identify how many Kb of memory are being used by the process
+			strcat(sys, aux); // With the process pid the program filters which process must have the monitored memory
+			strcat(sys, " | grep -i total"); // With the use of grep here, shows only the total memory used
 		}
-		while(i < 10) {// enquanto não se passarem 10 segundos, faça
-			system(sys); // chama o sistema e executa o comando determinado pelos if-else-if acima e executa-o
-			sleep(1); // espera um segundo
-			i++;//incrementa o i para chegar a condição de parada em 10 seg.
+		while(i < 10) {// While 10 seconds pass, do
+			system(sys); // Calls the system and executes the command determined by the if-else-if above and executes it
+			sleep(1); // Wait a second
+			i++;//Increments the i to reach the stop condition in 10 sec.
 		}
-		strcpy(sys, "kill ");// envia para sys um novo valor para o comando 'kill'
-		strcat(sys, aux); // com o pid do processo filho salvo em 'aux'
-		system(sys); // executa o comando 'kill *pid*' matando o processo filho após 10 segundos de execução
+		strcpy(sys, "kill ");// Sends to sys a new value for the command 'kill'
+		strcat(sys, aux); // With the child process pid saved in 'aux'
+		system(sys); // Executes the command 'kill * pid *' killing the child process after 10 seconds of execution
 	}
 	
-	perror ("Erro: ") ; /* execve não funcionou */
-	exit(0) ; /* encerra o processo com sucesso (código 0) */ 
+	perror ("Erro: ") ; /* execve did not work */
+	exit(0) ; /* Terminates the process successfully (code 0) */ 
 	return 0;
 }
